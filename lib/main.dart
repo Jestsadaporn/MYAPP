fa6e652534db586dc3d303641cae7ddcc2aa6f3e
Jestsadaporn <twentyakes2@gmail.com>
2024-11-18 21:24:12 +0700

    Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 // import 'package:myapp101/Worktab/Tabscreen.dart';
-import 'package:myapp101/screen.dart';
+// import 'package:myapp101/screen.dart';
+import 'package:myapp101/work.dart';
 // import 'package:myapp101/work.dart'; ด
 
 void main() {
   runApp(const MaterialApp(
     title: "WATTHANA",
-    home: Home(),
+    home: Work(),
   ));
 }
